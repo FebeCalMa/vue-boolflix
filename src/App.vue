9ee7d458a89957d7c9612853dc58ce096d039a4c
FebeCalMa <samcalma1@gmail.com>
2022-04-20 15:40:57 +0200

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/",
+    };
+  },
+  mounted() {
+    const query = "verde";
+    const params = {
+      query,
+      api_key: this.apiKey,
+      language: "it-IT",
+    };
+    axios
+      .get(this.apiUrl + "movie", { params })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
